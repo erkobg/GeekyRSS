b6ca497878f549e63b0aa1cdba6dd216ede1b8e8
unknown <I062115@SOFD60209670A.(none)>
2012-06-20 11:59:55 +0300

    v.1.0.0.0.1

--- src/com/android/geekyrss/ArticlesList.java
+++ src/com/android/geekyrss/ArticlesList.java
@@ -104,9 +104,7 @@ public class ArticlesList extends ListActivity {
 						progressDialog.dismiss();
 						runOnUiThread(new Runnable() {
 							public void run() {
-
 								fillData();
-
 							}
 						});
 
